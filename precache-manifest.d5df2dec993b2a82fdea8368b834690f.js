14891750b94775343d31fdf10ea06d27099f98d7
Karol <karolkolodziej01@gmail.com>
2020-01-17 13:37:58 +0100

    Updates

---
+++ precache-manifest.d5df2dec993b2a82fdea8368b834690f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e2b87183a262849b0b14c46a7b7ea9",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "31a4bd5ea007f92d1ad6",
+    "url": "/pokedex/static/css/main.bf62f1fc.chunk.css"
+  },
+  {
+    "revision": "f4cfc26f5dc2a7486366",
+    "url": "/pokedex/static/js/2.43c8b6f3.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/pokedex/static/js/2.43c8b6f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31a4bd5ea007f92d1ad6",
+    "url": "/pokedex/static/js/main.9c77310f.chunk.js"
+  },
+  {
+    "revision": "268252b7ef5b3d4cc4de",
+    "url": "/pokedex/static/js/runtime-main.8dad4ab6.js"
+  },
+  {
+    "revision": "4b41f8bbd0b4aaf0f1cb4e643df6a89c",
+    "url": "/pokedex/static/media/pokeLogo.4b41f8bb.png"
+  }
+]);

b4e452d912f0fbda891a20286836ca1d01dec5e4
Karol <karolkolodziej01@gmail.com>
2020-01-17 13:39:20 +0100

    style

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div className="headerLogo">
+        <div>
           <Header />
         </div>
         <div className="body">
